0516735949a5e5797294f2d23b52f53b875fad5d
hongl <hongl@chinaoly.com>
2018-09-05 21:37:58 +0800

    完善基本配置

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,14 @@ import Vuex from 'vuex'
 import state from './state'
 import mutations from './mutations'
 import actions from './actions'
-
+import user from './module/user'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
   mutations,
-  actions
+  actions,
+  modules:{
+    user
+  }
 })
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname,dir)
+
+const BASE_URL = process.env.NODE_ENV ==='procution' ? '/iview-admin/' : '/'
+
 module.exports = {
-  lintOnSave: false //取消Elint检测.
+  lintOnSave: false, //取消Elint检测
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  procutionSourceMap: false,//打包时不生成.map文件
+  devServer: {
+    proxy: 'http://localhost:4000'
+  } 
 }
