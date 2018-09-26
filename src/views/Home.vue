0230f17521a517af45e85de34bfbeca79388f4e8
hongl <hongl@chinaoly.com>
2018-09-26 21:43:37 +0800

    3d标签云

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,26 @@
     {{ food }}
     <hello-world msg="Welcome to Your Vue.js App"/>
     <button @click="increment">{{ count }}</button>
+    <div>
+    <div style="height: 400px; width: 400px;">
+    <div class="tagcloud">
+        <a href="javascript:;" @click="cFun">text</a>
+        <a href="#">text</a>
+        <a href="#">text</a>
+        <a href="#">text</a>
+        <a href="#">text</a>
+        <a href="#">text</a>
+        <a href="#">text</a>
+    </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '_c/HelloWorld.vue'
+require('TagCloud');
 
 export default {
   name: 'home',
@@ -16,19 +30,35 @@ export default {
     food: {
       type: String,
       default: 'apple2'
-    }
+    },
+  
   },
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data () {
     return {
-      count: 0
+      count: 0,
+      data: ['1212','131']
     }
+  },
+  mounted () {
+    tagcloud({
+      //参数名: 默认值
+      selector: ".tagcloud",  //元素选择器
+      fontsize: 16,       //基本字体大小
+      radius: 60,         //滚动半径
+      mspeed: "normal",   //滚动最大速度
+      ispeed: "normal",   //滚动初速度
+      direction: 135,     //初始滚动方向
+      keep: true          //鼠标移出组件后是否继续随鼠标滚动
+    })
   },
   methods: {
     increment () {
       this.count++
+    },
+    cFun () {
     }
   }
 }
---
+++ src/views/TagCloud.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <tagCloud :data=cc />
+  </div>
+</template>
+<script>
+import tagCloud from 'v-tag-cloud'
+export default {
+  components: {
+        tagCloud
+    },
+    data () {
+      cc:['1212','131']
+    }
+}
+</script>
+
